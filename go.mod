9a13ee6f2723c48e85dd2747c6f4d8531876144c
varunpv <vpv@confluent.io>
2021-10-17 10:34:30 +0530

    bcrypt password one-way hashing and comaparisio

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/varunpv/golang-webauth
 
 go 1.15
+
+require (
+	github.com/mailru/easyjson v0.7.7
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519
+)
--- main.go
+++ main.go
@@ -1,80 +1,40 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
 	"log"
-	"net/http"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
-type humanReal struct {
-	PhysicalBody string
-	PranicBody   []string
-	MindBody     string
-}
-type humanPerceived struct {
-	PhysicalBody string
-	MindBody     string `json:"mind"`
-}
-
 func main() {
-	// h1 := humanReal{
-	// 	PhysicalBody: "ATGTATGC",
-	// 	PranicBody:   []string{"?", "?", "?"},
-	// 	MindBody:     "state1",
-	// }
-	// h2 := humanPerceived{
-	// 	PhysicalBody: "TACG",
-	// 	MindBody:     "state2",
-	// }
-
-	// bh1, err := json.Marshal(h1)
-	// if err != nil {
-	// 	log.Panic(err)
-	// }
-	// fmt.Println(string(bh1))
-
-	// bh2, err := json.Marshal(h2)
-	// if err != nil {
-	// 	log.Panic(err)
-	// }
-	// fmt.Println(string(bh2))
-
-	// var rebornh1 humanReal
-
-	// err = json.Unmarshal(bh1, &rebornh1)
-	// if err != nil {
-	// 	log.Panic(err)
-	// }
-	// fmt.Println(rebornh1)
-
-	//encode and decode
-
-	http.HandleFunc("/", encoderhandler)
-	http.HandleFunc("/decode", decodehandler)
-	log.Fatal(http.ListenAndServe(":80", nil))
-}
-
-func encoderhandler(w http.ResponseWriter, r *http.Request) {
-	h2 := []humanReal{{
-		PhysicalBody: "ATGCTAGCC",
-		MindBody:     "state2",
-		PranicBody:   []string{"pranavayu", "udana vayu"},
-	},
-		{PhysicalBody: "TAC", MindBody: "state2", PranicBody: []string{"sama vayu"}},
+	pass := []byte("123")
+	hashPass, err := hashPassword(pass)
+	if err != nil {
+		log.Panic(err)
 	}
-	jencode := json.NewEncoder(w)
-	jencode.Encode(h2)
+	err = comparePassword(hashPass, pass)
+	if err != nil {
+		log.Println("Invalid password")
+	}
+	fmt.Println("Logged in!")
+
 }
 
-func decodehandler(w http.ResponseWriter, r *http.Request) {
-	var h2 []humanPerceived
+func hashPassword(password []byte) ([]byte, error) {
 
-	b := bytes.Buffer{}
-	b.ReadFrom(r.Body)
-	fmt.Println()
-	json.NewDecoder(r.Body).Decode(h2)
-	fmt.Println(h2)
+	hashedPassword, err := bcrypt.GenerateFromPassword(password, bcrypt.DefaultCost)
+	if err != nil {
+		return []byte{}, fmt.Errorf("Unable to hash password from bcrypt.GenerateFromPassword, %w", err)
+	}
+	return hashedPassword, nil
+}
+
+func comparePassword(hashedPassword, password []byte) error {
+	err := bcrypt.CompareHashAndPassword(hashedPassword, password)
+	if err != nil {
+		return fmt.Errorf("Unsuccessful comparasion: %w", err)
+	}
+	return nil
 
 }
