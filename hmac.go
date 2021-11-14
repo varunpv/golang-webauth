105f6d89053b3af114756542b075e0300d6bde50
varunpv <vpv@confluent.io>
2021-11-14 12:51:38 +0530

    Added the hmac file

---
+++ hmac.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"bufio"
+	"crypto/hmac"
+	"crypto/sha512"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var key = make([]byte, 64)
+
+func testhmac() {
+	fmt.Println("Enter your username")
+	s := bufio.NewScanner(os.Stdin)
+	s.Scan()
+	input := s.Bytes()
+	signedMessaged, _ := signMessage(input)
+
+	for {
+		fmt.Println("This is your Token. Keep it safe! -> ", signedMessaged)
+		fmt.Println("Did you note it down? YES or NO?")
+		s.Scan()
+		isnoted := strings.ToLower(s.Text())
+		if isnoted == "yes" {
+			break
+		}
+	}
+	fmt.Println("okay let me test you.... what is the token? ")
+	s.Scan()
+	inputeToken := getbyteslice(s.Text())
+	if ok, _ := validateMessage(inputeToken, input); ok {
+		fmt.Println("Nice !")
+	} else {
+		fmt.Println(" _|_")
+	}
+
+}
+
+func getbyteslice(s string) []byte {
+	var result []byte
+	stringnum := strings.Fields(s)
+	for _, is := range stringnum {
+		i, err := strconv.Atoi(is)
+		if err == nil {
+			result = append(result, byte(i))
+		}
+	}
+	return result
+}
+
+func signMessage(message []byte) ([]byte, error) {
+	h := hmac.New(sha512.New, key)
+	_, err := h.Write(message)
+	if err != nil {
+		return nil, fmt.Errorf("Error in SighMessage function : %w", err)
+	}
+	return h.Sum(nil), nil
+}
+
+func validateMessage(signedMessaged, message []byte) (bool, error) {
+	correctSign, err := signMessage(message)
+	if err != nil {
+		return false, fmt.Errorf("error in validating message %w", err)
+	}
+	isvalid := hmac.Equal(correctSign, signedMessaged)
+	return isvalid, nil
+}
--- main.go
+++ main.go
@@ -3,6 +3,6 @@ package main
 func main() {
 	// run the below line to use bcrypt to store and check for password.
 
-	testcrpyt()
-
+	//testcrpyt()
+	testhmac()
 }
