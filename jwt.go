c88aea0d7f2b174c317fab29ea58ab196154c520
varunpv <vpv@confluent.io>
2021-11-26 14:19:44 +0530

    Added parseToken function

--- jwt.go
+++ jwt.go
@@ -32,8 +32,25 @@ func createToken(u *UserClaims, key []byte) (string, error) {
 	return signedtoken, nil
 }
 
+func parseToken(signedToken string,key []byte) (*UserClaims, error) {
+	t,err:= jwt.ParseWithClaims(signedToken,&UserClaims{},func(t *jwt.Token) (interface{}, error) {
+		if t.Method.Alg() != jwt.SigningMethodHS256.Alg() {
+			return nil, fmt.Errorf("invalid signing Algorithm")
+		}
+		return key,nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("errro in parsetoken while paring token %w",err)
+	}
+	if !t.Valid {
+		return nil, fmt.Errorf("Token is not valid")
+	}
+	claims := t.Claims.(*UserClaims)
+	return claims,nil
+}
+
 func testJwt() {
-	key:=make([]byte,64)
+	key := make([]byte, 64)
 	rand.Read(key)
-	
+
 }
